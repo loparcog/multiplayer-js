4dc34f9bf2ee070d2bd41c13e241b07a9216e885
Giacomo Loparco <loparcog@mcmaster.ca>
2018-06-07 17:19:38 -0400

    Finished functioning of rock paper scissors, defined the two different players

--- server/rps-game.js
+++ server/rps-game.js
@@ -4,6 +4,7 @@ class RpsGame {
   constructor(p1, p2){
     this._players = [p1, p2];
     this._turns = [null, null];
+    this._scores = [0,0];
 
     this._sendToPlayers('Rock Paper Scissors Begin!');
 
@@ -25,18 +26,46 @@ class RpsGame {
   }
 
   _onTurn(playerIndex, turn){
-    this._turns[playerIndex] = turn;
-    this._sendToPlayer(playerIndex, `You selected ${turn}`);
-    this._checkGameOver();
+    if(this._turns[playerIndex] == null){
+      this._turns[playerIndex] = turn;
+      this._sendToPlayer(playerIndex, `You selected ${turn}`);
+      this._checkGameOver();
+    }
   }
 
   _checkGameOver(){
     const turns = this._turns;
 
     if(turns[0] && turns[1]){
-      this._sendToPlayers('Game Over!' + turns.join(' : '));
+      if (turns[0] == turns[1]){
+        this._sendToPlayers('Tie! Both chose ' + turns[0] + "!");
+      }
+      if (turns[0] == 'rock' && turns[1] == 'paper'){
+        this._sendToPlayers('P1 wins!');
+        this._scores[0]++;
+      }
+      if (turns[0] == 'rock' && turns[1] == 'scissors'){
+        this._sendToPlayers('P2 wins!');
+        this._scores[1]++;
+      }
+      if (turns[0] == 'paper' && turns[1] == 'rock'){
+        this._sendToPlayers('P2 wins!');
+        this._scores[1]++;
+      }
+      if (turns[0] == 'paper' && turns[1] == 'scissors'){
+        this._sendToPlayers('P1 wins!');
+        this._scores[0]++;
+      }
+      if (turns[0] == 'scissors' && turns[1] == 'rock'){
+        this._sendToPlayers('P1 wins!');
+        this._scores[0]++;
+      }
+      if (turns[0] == 'scissors' && turns[1] == 'paper'){
+        this._sendToPlayers('P2 wins!');
+        this._scores[1]++;
+      }
       this._turns = [null, null];
-      this._sendToPlayers('next!');
+      this._sendToPlayers('Scores: P1(' + this._scores[0] + '), P2(' + this._scores[1] + ')');
     }
   }
 }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 
-const RpsGame = require('./rps-game')
+const RpsGame = require('./rps-game');
+
+let P1 = 0;
+let P2 = 0;
 
 const app = express();
 
@@ -23,15 +26,25 @@ io.on('connection', (sock) => {
     //start game
     new RpsGame(waitingPlayer, sock);
     waitingPlayer = null;
+    P1 = sock;
   }
   else {
     waitingPlayer = sock;
     waitingPlayer.emit('message', 'Waiting for opponent...');
+    P2 = sock;
   }
 
   sock.on('message', (text) => {
     // sends to all
-    io.emit('message', text);
+    if (sock == P1){
+      io.emit('message', 'P1: ' + text);
+    }
+    else if (sock == P2){
+      io.emit('message', 'P2: ' + text);
+    }
+    else{
+      io.emit('message', 'dis shit broke');
+    }
   });
 
 });
